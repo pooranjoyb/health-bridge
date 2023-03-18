fc219dfcadea0d05dc749e8c04a0e7a4677aa35b
Madhurjya Dasgupta <dasguptamadhurjya@gmail.com>
2023-03-18 23:00:14 +0530

    Saved the videos to video dir

--- pages/textAnalysis.py
+++ pages/textAnalysis.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import secrets, string
+import os
+
+##Creating the temp directories
+if not os.path.exists('../assets/video'):
+    os.makedirs('../assets/video')
+if not os.path.exists('../assets/audio'):
+    os.makedirs('../assets/audio')
 
 st.set_page_config(page_title='Health Bridge', page_icon = '../assets/logo.png', initial_sidebar_state = 'auto')
 
@@ -8,8 +16,13 @@ txt = st.text_area('Enter your Symptoms to Get Started', ''' ''')
 
 user_video = st.file_uploader("Or Upload Your Video describing your Symptoms", type=['mov','mp4'])
 
+temp_name = '_'+''.join(secrets.choice(string.ascii_uppercase + string.digits)
+              for i in range(8))
+
 ### Reading Video from User
 
 if user_video is not None:
     video = user_video.read()
     st.video(video)
+    with open('../assets/video/temp'+temp_name+'.mp4', "wb") as temp_vid:
+        temp_vid.write(video)
