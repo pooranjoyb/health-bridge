d731099b7ad0346c7d0c861749c76c7b86a64594
pooranjoyb <pooranjoyb2016@gmail.com>
2023-03-19 23:47:44 +0530

    Added model function to return disease

--- health-bridge/models/model.py
+++ health-bridge/models/model.py
@@ -1,3 +1,11 @@
 import pickle
+import pandas as pd
+
 diagonosis_model = pickle.load(open('./telemedicine_model', 'rb'))
    
+
+def predict_disease(csvFile):
+    
+    df = pd.read_csv(csv_file)
+    predictions = diagonosis_model.predict(csvFile)
+    return decoded_final[predictions[0]]
