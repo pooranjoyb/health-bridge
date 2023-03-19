40b6625ae1037b37bcb49d41bb2c57641bf26649
pooranjoyb <pooranjoyb2016@gmail.com>
2023-03-19 11:58:18 +0530

    Added Choice to upload text ot vid

--- health-bridge/pages/textAnalysis.py
+++ health-bridge/pages/textAnalysis.py
@@ -13,27 +13,37 @@ if not os.path.exists('assets/audio'):
 st.set_page_config(page_title='Health Bridge', page_icon = 'assets/logo.png', initial_sidebar_state = 'auto')
 
 st.title("Welcome to Health Bridge")
+st.info('Please choose the option to communicate with our ML Model', icon="ℹ️")
 
-txt = st.text_area('Enter your Symptoms to Get Started', ''' ''')
+option = st.selectbox(
+    'How would you like to communicate ?',
+    ('Enter your Choice','Text', 'Video'))
 
-user_video = st.file_uploader("Or Upload Your Video describing your Symptoms", type=['mov','mp4'])
+st.write('You selected:', option)
 
-temp_name = '_'+''.join(secrets.choice(string.ascii_uppercase + string.digits)for i in range(8))
+userText = ""
+GenText = ""
+if option == 'Text':
+    userText = st.text_area('Enter your Symptoms to Get Started', ''' ''')
+elif option == 'Video':
+    user_video = st.file_uploader("Upload Your Video describing your Symptoms", type=['mov','mp4'])
 
-### Reading Video from User
+    temp_name = '_'+''.join(secrets.choice(string.ascii_uppercase + string.digits)for i in range(8))
 
-if user_video is not None:
-    video = user_video.read()
-    st.video(video)
-    with open('assets/video/temp'+temp_name+'.mp4', "wb") as temp_vid:
-        temp_vid.write(video)
+    ### Reading Video from User
 
-    clip = mp.VideoFileClip('assets/video/temp'+temp_name+'.mp4').subclip(0,15)
-    clip.audio.write_audiofile("assets/audio/temp"+temp_name+'.wav', codec='pcm_s16le')
+    if user_video is not None:
+        video = user_video.read()
+        st.video(video)
+        with open('assets/video/temp'+temp_name+'.mp4', "wb") as temp_vid:
+            temp_vid.write(video)
 
-    r = sr.Recognizer()
-    with sr.AudioFile("assets/audio/temp"+temp_name+'.wav') as source:
-        audio_text = r.listen(source)
-        text = r.recognize_google(audio_text, language='en-IN', show_all=True)
-        print('Converting audio transcripts into text ...')
-        print(text)
+        clip = mp.VideoFileClip('assets/video/temp'+temp_name+'.mp4').subclip(0,5)
+        clip.audio.write_audiofile("assets/audio/temp"+temp_name+'.wav', codec='pcm_s16le')
+
+        r = sr.Recognizer()
+        with sr.AudioFile("assets/audio/temp"+temp_name+'.wav') as source:
+            audio_text = r.listen(source)
+            genText = r.recognize_google(audio_text, language='en-IN', show_all=True)
+            print('Converting audio transcripts into text ...')
+            print(GenText)
